5a93989661d4f154782f5e58c4a7a36f633f64de
IceDBorn <51162078+IceDBorn@users.noreply.github.com>
2021-07-16 13:04:30 +0300

    Initial commit

---
+++ powercord.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+// Plugins
+git clone https://github.com/redstonekasi/theme-toggler
+git clone https://github.com/LandenStephenss/PowercordPluginDownloader.git
+git clone https://github.com/ploogins/PowercordThemeDownloader.git
+git clone https://github.com/Twizzer/relationship-notifier.git
+git clone https://github.com/A-Trash-Coder/Link-Preview-Utility.git
+git clone https://github.com/Juby210/message-link-embed.git
+git clone https://github.com/yatsukiko/powercord-jingle.git
+git clone https://github.com/LoggeL/powercord-duckduckgo.git
+git clone https://github.com/lorencerri/powercord-reverse-image-search.git
+git clone https://github.com/powercord-community/channel-typing.git
+git clone https://github.com/Puyodead1/powercord-ownertag.git
+git clone https://github.com/Rodentman87/reddit-parser.git
+git clone https://github.com/dutake/voice-chat-utilities.git
+git clone https://github.com/Vap0r1ze/vpc-shiki.git
+git clone https://github.com/PandaDriver156/Custom-Volume-Range.git
+git clone https://github.com/RazerMoon/vcTimer.git
+git clone https://github.com/tuanbinhtran/voice-user-count.git
+git clone https://github.com/Killerjet101/open-in-app.git
+git clone https://github.com/VenPlugs/Unindent.git
+git clone https://github.com/hyblocker/discord-theme-helper.git
+
+cd ../themes
+
+// Themes
+git clone https://github.com/DiscordStyles/RadialStatus.git
+git clone https://github.com/GhostMander/Dusk-to-Dawn-Settings.git
+git clone https://github.com/KraXen72/harmony-discord.git
+git clone https://github.com/snappercord/Settings-Icons.git
+git clone https://github.com/Discord-Theme-Addons/power-toasts
+git clone https://github.com/devinkid1/Pop/
+git clone https://github.com/NYRI4/Discolored.git
